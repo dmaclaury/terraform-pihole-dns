49067b1d2d382bf6a3d7f284b91756d446fcefaa
Daniel MacLaury <1966417+dmaclaury@users.noreply.github.com>
2023-02-16 14:32:58 -0800

    re-factor to map of strings

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "pihole_dns_record" "a_record" {
-  count  = length(var.a_records) > 0 ? length(var.a_records) : 0
-  domain = var.a_records[count.index].domain
-  ip     = var.a_records[count.index].ip_address
+  for_each = var.a_records
+  domain = each.key
+  ip     = each.value
 }
 
 resource "pihole_cname_record" "cname_record" {
-  count  = length(var.cname_records) > 0 ? length(var.cname_records) : 0
-  domain = var.cname_records[count.index].domain
-  target = var.cname_records[count.index].target
+  for_each = var.cname_records
+  domain = each.key
+  target = each.value
 }
--- variables.tf
+++ variables.tf
@@ -1,21 +1,11 @@
 variable "a_records" {
-  description = "A records to create in PiHole"
-  type = list(object(
-    {
-      domain     = string
-      ip_address = string
-    }
-  ))
-  default = []
+  description = "A records to create in PiHole. Should be a map of strings, the key name will be used for domain, and value for IP address."
+  type        = map(string)
+  default     = {}
 }
 
 variable "cname_records" {
-  description = "CNAME records to create"
-  type = list(object(
-    {
-      domain = string
-      target = string
-    }
-  ))
-  default = []
+  description = "CNAME records to create. Should be a map of strings, the key name will be used for domain, and value for target."
+  type        = map(string)
+  default     = {}
 }
