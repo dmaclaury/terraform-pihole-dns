b0ee740f952ea667029ed35a312edfd0f500ddcb
Daniel MacLaury <1966417+dmaclaury@users.noreply.github.com>
2023-02-16 14:30:35 -0800

    Update examples

--- examples/variable_map/main.tf
+++ examples/variable_map/main.tf
@@ -19,22 +19,12 @@ module "pihole_internal_dns" {
 
 variable "a_records" {
   description = "A records to create in PiHole"
-  type = list(object(
-    {
-      domain     = string
-      ip_address = string
-    }
-  ))
-  default = []
+  type        = map(string)
+  default     = {}
 }
 
 variable "cname_records" {
   description = "CNAME records to create"
-  type = list(object(
-    {
-      domain = string
-      target = string
-    }
-  ))
-  default = []
+  type        = map(string)
+  default     = {}
 }
--- examples/variable_map/terraform.tfvars
+++ examples/variable_map/terraform.tfvars
@@ -1,21 +1,9 @@
-a_records = [
-  {
-    domain     = "arecord1"
-    ip_address = "10.11.12.13"
-  },
-  {
-    domain     = "arecord2.example.com"
-    ip_address = "10.11.12.14"
-  }
-]
+a_records = {
+  "arecord1" = "10.11.12.13",
+  "arecord2" = "10.11.12.14"
+}
 
-cname_records = [
-  {
-    domain = "cnamerecord1"
-    target = "cname.example.com"
-  },
-  {
-    domain = "cnamerecord2"
-    target = "arecord2.example.com"
-  }
-]
+cname_records = {
+  "cname1" = "arecord1",
+  "cname2" = "arecord2",
+}
